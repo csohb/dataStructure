c6a0d1fb7d58459b8e43f17909abe1d398cd4e3d
winter.kwon <winter.kwon@gbike.io>
2024-03-21 21:19:52 +0900

    implementing doubleLinkedList

---
+++ linkedList/doubleLinkedList/doubleLinkedList.go
@@ -0,0 +1,146 @@
+package doubleLinkedList
+
+type Node[T any] struct {
+	next *Node[T]
+	prev *Node[T]
+
+	Value T
+}
+
+type LinkedList[T any] struct {
+	root  *Node[T]
+	tail  *Node[T]
+	count int
+}
+
+func (l *LinkedList[T]) PushBack(val T) {
+	n := &Node[T]{
+		Value: val,
+	}
+
+	if l.tail == nil {
+		l.root = n
+		l.tail = n
+		l.count = 1
+		return
+	}
+
+	l.tail.next = n
+	n.prev = l.tail
+	l.tail = n
+	l.count++
+}
+
+func (l *LinkedList[T]) Front() *Node[T] {
+	return l.root
+}
+func (l *LinkedList[T]) Back() *Node[T] {
+	return l.tail
+}
+
+func (l *LinkedList[T]) Count() int {
+	return l.count
+}
+func (l *LinkedList[T]) GetAt(index int) *Node[T] {
+	if index >= l.Count() {
+		return nil
+	}
+	i := 0
+	for node := l.root; node != nil; node = node.next {
+		if i == index {
+			return node
+		}
+		i++
+	}
+	return nil
+}
+
+func (l *LinkedList[T]) PushFront(val T) {
+	n := &Node[T]{Value: val}
+	if l.root == nil {
+		l.root = n
+		l.tail = n
+		l.count = 1
+		return
+	}
+
+	l.root.prev = n
+	n.next = l.root
+	l.root = n
+	l.count++
+}
+
+func (l *LinkedList[T]) IsInclude(node *Node[T]) bool {
+	newNode := l.root
+	for ; newNode != nil; newNode = newNode.next {
+		if newNode == node {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *LinkedList[T]) InsertAfter(node *Node[T], val T) {
+	if !l.IsInclude(node) {
+		return
+	}
+
+	n := &Node[T]{Value: val}
+
+	if node == l.tail {
+		l.PushBack(val)
+		return
+	}
+
+	originNext := node.next
+	node.next = n
+
+	n.next = originNext
+	n.prev = node
+
+	originNext.prev = n
+
+	l.count++
+}
+
+func (l *LinkedList[T]) InsertBefore(node *Node[T], val T) {
+	if !l.IsInclude(node) {
+		return
+	}
+
+	n := &Node[T]{Value: val}
+
+	if node == l.root {
+		l.PushFront(val)
+	}
+
+	originPrev := node.prev
+	node.prev = n
+
+	n.prev = originPrev
+	n.next = node
+
+	originPrev.next = n
+	l.count++
+}
+
+func (l *LinkedList[T]) PopFront() *Node[T] {
+	if l.root == nil {
+		return nil
+	}
+
+	n := l.root
+
+	l.root = n.next
+
+	if l.root != nil {
+		l.root.prev = nil
+	} else {
+		l.tail = nil
+	}
+	n.next = nil
+
+	l.count--
+	return n
+
+}
---
+++ linkedList/doubleLinkedList/doubleLinkedList_test.go
@@ -0,0 +1,127 @@
+package doubleLinkedList
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPushBack(t *testing.T) {
+	var l LinkedList[int]
+
+	assert.Nil(t, l.root)
+	assert.Nil(t, l.tail)
+
+	l.PushBack(1)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 1, l.Back().Value)
+	assert.Equal(t, 1, l.Front().Value)
+
+	l.PushBack(2)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 1, l.Front().Value)
+	assert.Equal(t, 2, l.Back().Value)
+
+	l.PushBack(3)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 1, l.Front().Value)
+	assert.Equal(t, 3, l.Back().Value)
+
+	assert.Equal(t, 3, l.Count())
+
+	assert.Nil(t, l.GetAt(3))
+}
+
+func TestPushFront(t *testing.T) {
+	var l LinkedList[int]
+
+	assert.Nil(t, l.root)
+	assert.Nil(t, l.tail)
+
+	l.PushFront(1)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 1, l.Back().Value)
+	assert.Equal(t, 1, l.Front().Value)
+
+	l.PushFront(2)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 2, l.Front().Value)
+	assert.Equal(t, 1, l.Back().Value)
+
+	l.PushFront(3)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 3, l.Front().Value)
+	assert.Equal(t, 1, l.Back().Value)
+
+	assert.Equal(t, 3, l.Count())
+
+	assert.Nil(t, l.GetAt(3))
+}
+
+func TestInsertAfter(t *testing.T) {
+	var l LinkedList[int]
+
+	assert.Nil(t, l.root)
+	assert.Nil(t, l.tail)
+
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 1, l.Front().Value)
+	assert.Equal(t, 3, l.Back().Value)
+
+	node := l.GetAt(1)
+	l.InsertAfter(node, 4)
+
+	assert.Equal(t, 4, l.GetAt(2).Value)
+	assert.Equal(t, 3, l.Back().Value)
+	assert.Equal(t, 4, node.next.Value)
+	assert.Equal(t, 3, node.next.next.Value)
+	assert.Equal(t, 4, node.next.next.prev.Value)
+}
+func TestInsertBefore(t *testing.T) {
+	var l LinkedList[int]
+
+	assert.Nil(t, l.root)
+	assert.Nil(t, l.tail)
+
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	assert.NotNil(t, l.root)
+	assert.Equal(t, 1, l.Front().Value)
+	assert.Equal(t, 3, l.Back().Value)
+
+	node := l.GetAt(1)
+	l.InsertBefore(node, 4)
+
+	assert.Equal(t, 4, l.GetAt(1).Value)
+	assert.Equal(t, 3, l.Back().Value)
+	assert.Equal(t, 4, node.prev.Value)
+	assert.Equal(t, 1, node.prev.prev.Value)
+	assert.Equal(t, 4, node.prev.prev.next.Value)
+}
+
+func TestPopFront(t *testing.T) {
+	var l LinkedList[int]
+
+	assert.Nil(t, l.root)
+	assert.Nil(t, l.tail)
+
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	n := l.PopFront()
+	assert.Equal(t, 1, n.Value)
+	assert.Equal(t, 2, l.root.Value)
+
+}
